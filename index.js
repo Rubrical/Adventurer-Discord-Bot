66c286941f95f669552b7ba5994e4b3ed3300f30
Rubrical <filipesalviano@proton.me>
2023-09-08 18:26:45 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+const dotenv = require('dotenv');
+dotenv.config();
+const { TOKEN } = process.env;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, c => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(TOKEN)
